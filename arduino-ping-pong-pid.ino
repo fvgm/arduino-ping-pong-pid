0798cc1a9620647344e521e3699987eb48bb7725
fvgm <fv.magro@gmail.com>
2018-08-19 00:23:18 -0300

    Initial commit

---
+++ arduino-ping-pong-pid.ino
@@ -0,0 +1,54 @@
+/* This example shows how to use continuous mode to take
+range measurements with the VL53L0X. It is based on
+vl53l0x_ContinuousRanging_Example.c from the VL53L0X API.
+
+The range readings are in units of mm. */
+
+#include <Wire.h>
+#include <VL53L0X.h>
+
+VL53L0X sensor;
+int distance = 0;
+int tubeSize = 101;
+
+int byteReceived = 0;
+int pwmPin = 9;
+//int deadZone = 55;
+//int output = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+
+  sensor.init();
+  sensor.setTimeout(500);
+
+  // Start continuous back-to-back mode (take readings as
+  // fast as possible).  To use continuous timed mode
+  // instead, provide a desired inter-measurement period in
+  // ms (e.g. sensor.startContinuous(100)).
+  sensor.startContinuous();
+
+  pinMode(pwmPin, OUTPUT);
+
+}
+
+void loop() {
+  distance = tubeSize -(sensor.readRangeContinuousMillimeters()/10);
+  Serial.write(distance);
+  
+  if (sensor.timeoutOccurred()) { 
+    //Serial.print(" TIMEOUT"); 
+  }
+
+  //output = map(byteReceived,0,100,deadZone,255);
+  analogWrite(pwmPin, byteReceived);
+}
+
+void serialEvent() {
+  if(Serial.available()) {
+    byteReceived = Serial.read();
+    //byteSent = byteReceived;
+  }
+}
+
